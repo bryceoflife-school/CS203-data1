c5e896c3f621667e783b905ce2f9692a39f98a01
Bryce Daniel <brycerdaniel@gmail.com>
2014-09-14 17:50:35 -0400

    Random generation for trees is functional
    Random generation works and union is functional :)

--- FiniteSet.java
+++ FiniteSet.java
@@ -7,38 +7,32 @@ public class FiniteSet{
     public static int randInt( int min, int max ) {
         return new Random().nextInt((max - min) + 1) + min; }
     
+    public static BinaryTree randomTree(int nodes){
+        if (nodes == 0) {
+            return new Leaf();
+        } else {
+            return new Branch(randInt(0,100), randomTree(nodes - 1), randomTree(nodes - 1));
+        }
+        
+    }
+    
+    
     public static void unionChecker(int times){
         for(int i = 0; i < times; i++){
+            int elt = randInt(0,100);
+            int nodes = randInt(0, 10);
             
-            BinaryTree firstHalf = new Leaf();
-            BinaryTree secondHalf = new Leaf();
             
-            for(int j = 0; j < 30; j++){
-                firstHalf.add(randInt(0,100));
-                
-                
-            }
+            BinaryTree firstHalf = randomTree(nodes);
+            BinaryTree secondHalf = randomTree(nodes);
             
-            for(int j = 0; j < 30; j++){
-                secondHalf.add(randInt(0,100));
-            }
+            System.out.println("firstHalf Cardinality: " + firstHalf.cardinality());
+            System.out.println("secondHalf Cardinality: " + secondHalf.cardinality());
+
             
-            BinaryTree wholeTree;
-            wholeTree = firstHalf.union(secondHalf);
-            /*
-            for(int j = 0; j < 100; j++){
-                if((firstHalf.member(j)!= wholeTree.member(i))||
-                   (secondHalf.member(i)!= wholeTree.member(i))){
-                    System.out.println("HALP");
-                }
-             }
-             */
-//            System.out.println(firstHalf.cardinality());
-//            System.out.println(secondHalf.cardinality());
-            if (wholeTree == null){
-            System.out.println("WTF NULL");
-            }
-            
+            BinaryTree wholeTree = firstHalf.union(secondHalf);
+            System.out.println("WholeTree Cardinality: " + wholeTree.cardinality());
+            System.out.println(" ");
         }
     }
     
@@ -63,33 +57,40 @@ public class FiniteSet{
                                            (new Branch(11)));
         
         // Tests for Cardinality()
+        System.out.println(" ");
+        System.out.println("Tests for cardinality()");
         System.out.println("Cardinality of t1 is: " + t1.cardinality() + " expected " + 5);
         System.out.println("Cardinality of t2 is: " + t2.cardinality() + " expected " + 6);
         
         // Tests for isEmptyHuh()
+        System.out.println(" ");
+        System.out.println("Tests for isEmptyHuh()");
         System.out.println(t1.isEmptyHuh() + " expected " + false);
         System.out.println(empty.isEmptyHuh() + " expected " + true);
         
         // Tests for member()
+        System.out.println(" ");
+        System.out.println("Tests for member()");
         System.out.println(t1.member(9) + " expected " + true);
         System.out.println(t2.member(9) + " expected " + true);
         System.out.println(empty.member(9) + " expected " + false);
         
         
         // Tests for add()
+        System.out.println(" ");
+        System.out.println("Tests for add()");
         System.out.println(t1.member(6) + " expected " + false);
         System.out.println("Cardinality of t1 is: " + t1.add(6).cardinality() + " expected " + 6);
         System.out.println(t1.add(6).member(6) + " expected " + true);
         
-        
-//        System.out.println(new Random().nextInt(100));
-//        System.out.println(randInt(0,10));
-        
-        BinaryTree thirdHalf = firstHalf.union(secondHalf);
-        System.out.println(thirdHalf.cardinality());
-        
-        
         // Tests for union()
+        System.out.println(" ");
+        System.out.println("Tests for Union() of manually generated trees");
+        BinaryTree thirdHalf = firstHalf.union(secondHalf);
+        System.out.println(thirdHalf.cardinality() + " expected " + 5);
+        
+        System.out.println(" ");
+        System.out.println("Tests for Union() of randomly generated trees");
         unionChecker(5);
         
         
