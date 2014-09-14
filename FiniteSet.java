1f6b910e423411496f0332575484fd5ac361dc30
Bryce Daniel <brycerdaniel@gmail.com>
2014-09-14 15:16:44 -0400

    Add function appears to be working!
    Add() seems to be working and it changes the cardinality but for some
    reason when member is called, to look for the added elt, it is
    returning false.

--- FiniteSet.java
+++ FiniteSet.java
@@ -5,26 +5,33 @@ public class FiniteSet{
     
         BinaryTree empty = new Leaf();
         
-        BinaryTree t1 = new Branch(9, empty, empty);
-        BinaryTree t2 = new Branch(11, t1, empty);
-        BinaryTree t3 = new Branch(7, empty, t2);
+        BinaryTree t1 = new Branch(9,
+                                   (new Branch(7, new Leaf(), new Branch(8))),
+                                   (new Branch(11, new Leaf(), new Branch(12))));
+        
+        
+        
+        
+        BinaryTree t2 = new Branch(13, t1, empty);
         
         // Tests for Cardinality()
-        System.out.println("Cardinality of t1 is: " + t1.cardinality() + " expected " + 1);
-        System.out.println("Cardinality of t2 is: " + t2.cardinality() + " expected " + 2);
+        System.out.println("Cardinality of t1 is: " + t1.cardinality() + " expected " + 5);
+        System.out.println("Cardinality of t2 is: " + t2.cardinality() + " expected " + 6);
         
         // Tests for isEmptyHuh()
-        System.out.println(t1.isEmptyHuh());
-        System.out.println(t2.isEmptyHuh());
+        System.out.println(t1.isEmptyHuh() + " expected " + false);
+        System.out.println(empty.isEmptyHuh() + " expected " + true);
         
         // Tests for member()
-        System.out.println(t1.member(9));
-        System.out.println(t2.member(9));
-        System.out.println(t3.member(9));
+        System.out.println(t1.member(9) + " expected " + true);
+        System.out.println(t2.member(9) + " expected " + true);
+        System.out.println(empty.member(9) + " expected " + false);
+        
         
         // Tests for add()
-        System.out.println(t1.add(12));
-        System.out.println(t1.member(10));
+        System.out.println(t1.member(6) + " expected " + false);
+        System.out.println("Cardinality of t1 is: " + t1.add(6).cardinality() + " expected " + 6);
+        System.out.println(t1.member(6) + " expected " + true);
      
         
            }
